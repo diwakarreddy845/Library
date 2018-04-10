5cb66f0191b32045e14cd67948c4e24ccf5bc43e
Diwakar Reddy <diwakar.gudisi@experian.com>
2018-04-10 18:08:18 +0800

    New component is added

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -3,11 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const appRoutes: Routes = [
+  {  
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  } ];
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
