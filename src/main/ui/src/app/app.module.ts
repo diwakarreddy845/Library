027cec19e361f7ba864a1f9d4f0a025c17f1ccfd
Diwakar, Gudisi <diwakar.gudisi@experian.com>
2018-04-12 18:04:26 +0800

    bootstrap is added

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
+  {  
+    path: 'aboutus',
+    component: AboutusComponent
+  },
   {  
     path: 'contact',
     component: ContactComponent
@@ -16,15 +21,17 @@ const appRoutes: Routes = [
     path: 'home',
     component: HomeComponent
   } ];
-  
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    AboutusComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
