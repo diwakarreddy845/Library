cb18a26063f95394311ccb011e74a368c874d0d4
Diwakar Reddy <diwakar.gudisi@experian.com>
2018-04-19 16:59:15 +0800

    App outline is added

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -3,35 +3,40 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutusComponent } from './aboutus/aboutus.component';
-import { RouterModule, Routes } from '@angular/router';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
-const appRoutes: Routes = [
-  {  
-    path: 'aboutus',
-    component: AboutusComponent
-  },
-  {  
-    path: 'contact',
-    component: ContactComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  } ];
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { BooksComponent } from './books/books.component';
+import { BooksListComponent } from './books/books-list/books-list.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { BookComponent } from './books/books-list/book/book.component';
+import { AuthorsListComponent } from './authors/authors-list/authors-list.component';
+import { AuthorComponent } from './authors/authors-list/author/author.component';
+import { AuthorDetailsComponent } from './authors/author-details/author-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+
+
+const appRoutes: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ContactComponent,
-    AboutusComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent,
+    FooterComponent,
+    AuthorsComponent,
+    BooksComponent,
+    BooksListComponent,
+    BookDetailsComponent,
+    BookComponent,
+    AuthorsListComponent,
+    AuthorComponent,
+    AuthorDetailsComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,4 +45,5 @@ const appRoutes: Routes = [
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
